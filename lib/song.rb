8048397474e89ff2d29d3b53c3c0d7a8dea1a3c4
sopharsogood <justanothersophie@gmail.com>
2021-07-22 22:24:22 -0400

    commit tree 20edae9ac9aaca5589fb156c1d4a95881f5e6c16

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +12,51 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = Song.new
+    @@all << new_song
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.create
+    new_song.name = name
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    return self.find_by_name(name) if self.find_by_name(name)
+    self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    split_filename = filename.split(/[-\.]/)
+    new_song = self.new_by_name(split_filename[1].strip)
+    new_song.artist_name = split_filename[0].strip
+    new_song
+  end
+
+  def self.create_from_filename(filename)
+    new_song = self.new_from_filename(filename)
+    @@all << new_song
+    new_song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
